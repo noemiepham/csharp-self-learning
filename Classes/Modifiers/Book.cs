08af6a2bce6418ea512e4c3329d13e7a91e69ca3
noemiepham <huynhtieuquy@gmail.com>
2022-11-22 02:38:17 +0100

    Access modifiers

--- Classes/Modifiers/Book.cs
+++ Classes/Modifiers/Book.cs
@@ -7,15 +7,15 @@ namespace Modifiers
     {
         // TODO: "public" members and methods can be accessed by any other code
         // Note: this is *NOT* the right way to expose internal data
-        string _name;
+        public string _name;
 
         // TODO: "protected" members can only be accessed by the class or a 
         // derived class from this one
-        string _author;
+        public string _author;
 
         // TODO: "private" is the default and can only be accessed by code 
         // within the class itself
-        int _pagecount;
+        private int _pagecount;
 
         public Book(string name, string author, int pages) {
             _name = name;
