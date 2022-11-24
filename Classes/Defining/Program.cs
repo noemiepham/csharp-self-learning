56a280c384eed08275d2fb260068b596426f2103
noemiepham <huynhtieuquy@gmail.com>
2022-11-24 01:24:53 +0100

    Defiing

--- Classes/Defining/Program.cs
+++ Classes/Defining/Program.cs
@@ -7,13 +7,17 @@ namespace Defining
         static void Main(string[] args)
         {
             // TODO: Create new object instances using the "new" operator
-
+            Book b1 = new Book("war and peace","Leo", 825);
+            Book b2 = new Book("The start war", " Jorge Luca", 468);
             
             // TODO: Call a method on the object
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
 
 
             // TODO: try to set one of the properties -- 
             // this will result in an error
+            //b1._name = "Aldous Huxley";
 
         }
     }
